ab8e17420c1b88a733e0ebe76b648e7efaea346f
Albert Didde <albertdidde@alberts-laptop.mynetworksettings.com>
2025-01-31 21:39:59 -0500

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Profile from './pages/profile/profile';
+import Counter from './features/counter/counter'
+
+function App() {
+  return (
+    <div className="App">
+      <Profile></Profile>
+    </div>
+  );
+}
+
+export default App
---
+++ src/pages/profile/profile.tsx
@@ -0,0 +1,25 @@
+import profile from '../../assets/profile.jpg'
+import './profile.css'
+
+function Profile() {
+  return (
+    <div className="profile-container">
+      <img
+        src={profile}
+        alt="Profile"
+        className="profile-picture"
+      />
+      <div className="dialogue-container">
+        <p className="name">Albert Didde</p>
+        <p className="dialogue">
+          Hi, I'm Albert! I'm a full-stack engineer currently working at Bloomberg, building and creating innovative solutions. 
+          Outside of work, I’m passionate about staying fit—whether it’s through weight training or exploring new fitness routines. 
+          I also love photography, capturing moments and scenes that inspire me, and cooking up delicious meals in the kitchen. 
+          Balancing both my professional and personal passions is something I really enjoy!
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
