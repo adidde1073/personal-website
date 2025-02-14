ea460f8e6ab132a5ab2d6b9769d9be6fa3bd6665
Albert Didde <albertdidde@alberts-laptop.mynetworksettings.com>
2025-02-14 14:04:46 -0500

    chore: cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import './index.css'
 import App from './App.tsx'
 import { store } from './app/store'
 import { Provider } from 'react-redux'
-import { BrowserRouter, HashRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
