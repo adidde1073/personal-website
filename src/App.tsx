868a54860128d511d43557a5fee35506ddb1398f
Albert Didde <albertdidde@alberts-laptop.mynetworksettings.com>
2025-02-14 13:26:37 -0500

    chore: update base path in vite.config for routing when using GitHub pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route } from "react-router"
 import './App.css'
-import Profile from './pages/profile/profile';
-import About from "./pages/about/about";
+import Profile from './pages/profile/profile'
+import About from "./pages/about/about"
 // import Counter from './features/counter/counter'
 
 function App() {
   return (
     <div className="App">
       <Routes>
-      <Route path="/" element={<Profile />} />
-      <Route path="/about" element={<About />}/>
-    </Routes>
+        <Route path="/" element={<Profile />} />
+        <Route path="/about" element={<About />}/>
+      </Routes>
     </div>
   );
 }
