3c95d6517edce1b43e98bfb5dfe90bcc526bf5ef
Albert Didde <albertdidde@alberts-laptop.mynetworksettings.com>
2025-02-16 16:13:40 -0500

    feat: Photography portfolio page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import { Routes, Route } from "react-router"
 import './App.css'
+
+/**
+ * Import Pages Block
+ */
 import About from "./pages/AboutPage/AboutPage"
 import HomePage from "./pages/HomePage/HomePage"
+import PhotographyPage from "./pages/PhotographyPage/PhotographyPage"
+
+/**
+ * Import Components Block
+ */
 import Navbar from './components/navbar'
-// import Counter from './features/counter/counter'
 
 function App() {
   return (
@@ -12,6 +20,7 @@ function App() {
       <Routes>
         <Route path="/about" element={<About />}/>
         <Route path="/" element={<HomePage />}/>
+        <Route path="/photography" element={<PhotographyPage />}/>
       </Routes>
     </div>
   );
---
+++ src/pages/PhotographyPage/PhotographyPage.tsx
@@ -0,0 +1,12 @@
+import { CImage } from '@coreui/react'
+import './PhotographyPage.css'
+
+function PhotographyPage() {
+  return (
+    <div className="phtoContainer">
+      <h1 className='phtoHeader'>My Photo Portfolio</h1>
+    </div>
+  )
+}
+
+export default PhotographyPage
